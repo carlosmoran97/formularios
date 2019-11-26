a0bdc5b2de55be02c9f991228d9c62aef291994b
Carlos Morán <moran_krlos@hotmail.com>
2019-11-26 16:57:20 -0600

    formularios

---
+++ android/app/src/main/kotlin/com/rinnovo/formulario/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rinnovo.formulario
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
